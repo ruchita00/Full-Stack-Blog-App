cce0cc218943fc15642a0763166bc08123f87d15
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-25 09:44:34 +0530

    logic to store the image into public folder

--- next-blog-app/app/api/blog/route.js
+++ next-blog-app/app/api/blog/route.js
@@ -1,5 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
-
+import { writeFile } from "fs/promises";
 const { NextResponse } = require("next/server");
 
 const LoadDB = async () => {
@@ -8,4 +8,18 @@ const LoadDB = async () => {
 LoadDB();
 export async function GET(request) {
   return NextResponse.json({ msg: "API Working" });
+}
+export async function POST(request) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  //logic to store the image into public folder
+  const image = formData.get("image");
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgURL = `/${timestamp}_${image.name}`;
+  console.log("img url", imgURL);
+  return NextResponse.json({ imgURL });
 }
