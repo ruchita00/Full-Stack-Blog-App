b68668c37d9e37b9f31bfd127e5f75db6546dfd3
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-25 20:55:47 +0530

    post api hit successfully

--- next-blog-app/app/admin/addProduct/page.jsx
+++ next-blog-app/app/admin/addProduct/page.jsx
@@ -1,8 +1,117 @@
 "use client";
-import React from "react";
+import { assets } from "@/Assets/assets";
+import axios from "axios";
+import Image from "next/image";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const page = () => {
-  return <div>page</div>;
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    category: "Startup",
+    author: "Alex Bennett",
+    authorImg: "/author_img.png",
+  });
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("category", data.category);
+    formData.append("author", data.author);
+    formData.append("authorImg", data.authorImg);
+    formData.append("image", data.image);
+
+    let response = await fetch("/api/blog", {
+      method: "POST",
+      headers: {},
+      body: formData,
+    });
+    response = await response.json();
+    if (response?.success) {
+      toast.success(response?.msg);
+      setData({
+        title: "",
+        description: "",
+        category: "Startup",
+        author: "Alex Bennett",
+        authorImg: "/author_img.png",
+      });
+      setImage(false);
+    } else {
+      toast.error("error", response.error);
+    }
+  };
+  return (
+    <>
+      <form
+        className="pt-5 px-5 sm:pt-12 sm:pl-16 mb-5"
+        onSubmit={onSubmitHandler}
+      >
+        <p className="text-xl">Upload thumbnail</p>
+        <label htmlFor="image">
+          <Image
+            className="mt-4"
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            width={140}
+            height={70}
+            alt=""
+          />
+        </label>
+        <input
+          onChange={(e) => setImage(e.target.files[0])}
+          type="file"
+          id="image"
+          hidden
+          required
+        />
+        <p className="text-xl mt-4">Blog title</p>
+        <input
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          type="text"
+          placeholder="Type here"
+          name="title"
+          value={data.title}
+          required
+          onChange={onChangeHandler}
+        />{" "}
+        <p className="text-xl mt-4">Blog description</p>
+        <textarea
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          type="text"
+          placeholder="Write content here"
+          rows={6}
+          value={data.description}
+          name="description"
+          onChange={onChangeHandler}
+          required
+        />{" "}
+        <p className="text-xl mt-4">Blog category</p>
+        <select
+          onChange={onChangeHandler}
+          value={data.catgory}
+          name="category"
+          className="w-40 mt-4 px-4 py-3 border text-gray-500"
+        >
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <br />
+        <button type="submit" className="mt-8 w-40 h-12 bg-black text-white">
+          ADD
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default page;
--- next-blog-app/app/api/blog/route.js
+++ next-blog-app/app/api/blog/route.js
@@ -5,36 +5,39 @@ const { NextResponse } = require("next/server");
 
 export async function GET(request) {
   await connectDB();
-
   return NextResponse.json({ msg: "API Working" });
 }
+
 export async function POST(request) {
   await connectDB();
 
   const formData = await request.formData();
-  console.log("formData123", formData);
+  const image = formData.get("image");
   const timestamp = Date.now();
 
   //logic to store the image into public folder
-  const image = formData.get("image");
-  const imageByteData = await image.arrayBuffer();
-  const buffer = Buffer.from(imageByteData);
-  const path = `./public/${timestamp}_${image.name}`;
-  await writeFile(path, buffer);
-  const imgURL = `/${timestamp}_${image.name}`;
+
+  let imgURL = null;
+  // if (image !== undefined) {
+  //   const imageByteData = await image?.arrayBuffer();
+  //   const buffer = Buffer.from(imageByteData);
+  //   const path = `./public/${timestamp}_${image?.name}`;
+  //   await writeFile(path, buffer);
+  //   imgURL = `/${timestamp}_${image?.name}`;
+  // }
 
   const blogData = {
     title: `${formData.get("title")}`,
     description: `${formData.get("description")}`,
     category: `${formData.get("category")}`,
     author: `${formData.get("author")}`,
-    image: `${imgURL}`,
+    image: `${imgURL ?? "sample.png"}`,
     authorImg: `${formData.get("authorImg")}`,
   };
 
-  //logic to store blog data and blog model into database
   await BlogModel.create(blogData);
-  console.log("Blog Saved");
-
-  return NextResponse.json({ success: true, msg: "Blog Added" });
+  return NextResponse.json(
+    { success: true, msg: "Blog Added" },
+    { status: 200 }
+  );
 }
